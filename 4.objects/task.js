0863c4f9d15024f03cf9e65873f31e0a6904ec92
margo-madeira <ligastaeva@mail.ru>
2023-09-14 17:24:28 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+let student1 = new Student();
+let student2 = new Student();
+let student3 = new Student();
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if((this.marks in Student) === true) {
+    this.marks = [...marksToAdd];
+  }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function (...marks) {
+  let sum = 0;
+    if("this.marks" in Student === false || this.marks.length === 0) {
+      return 0;
+    } else {
+        for(let i = 0; i < this.marks.length; i++) {
+          sum += this.marks[i];
+        }
+      }
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
